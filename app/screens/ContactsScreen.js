da88025183e010b8ac96bc3c27ac1084aa1268a6
Martina <martinacamilaleon@gmail.com>
2024-12-07 17:19:21 -0500

    cambios estudiante 1 modificar app.js

--- app/screens/ContactsScreen.js
+++ app/screens/ContactsScreen.js
@@ -6,7 +6,7 @@ export const Contacts = ({navigation}) => {
         <Button
             title='HOME'
             onPress={()=>{
-                navigation.navigate('WellcomeAlejandro');
+                navigation.navigate('Wellcome_Martina');
             }}
         />
     </View>
